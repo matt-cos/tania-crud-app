788ce73f7de60fc3a206618ca34ca5c4d7403a86
Matt Cosentino <matt@learnedmedia.com>
2018-01-05 18:30:26 -0500

    first commit

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php include "templates/header.php" ?>
+
+<ul>
+	<li><a href="create.php"><strong>Create</strong></a> - add a user</li>
+	<li><a href="read.php"><strong>Read</strong></a> - find a user</li>
+</ul>
+<!-- https://www.taniarascia.com/create-a-simple-database-app-connecting-to-mysql-with-php/ -->
+
+<?php include "templates/footer.php" ?>
