6cc75de6447f0c52c4f47599d81b144d07cd7fda
Matt Cosentino <matt@learnedmedia.com>
2018-01-19 22:08:12 -0500

    dynamic page title

--- public/templates/header.php
+++ public/templates/header.php
@@ -4,7 +4,8 @@
 		<meta charset="utf-8">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Simple Database App</title>
+		<!-- <title>Simple Database App</title> -->
+		<title>Run Tracker | <?php echo $pageName; ?></title>
 		<link rel="stylesheet" href="css/dashboard-styles.css">
 		<link rel="stylesheet" href="css/style.css">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
