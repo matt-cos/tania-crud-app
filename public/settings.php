6d5f70e0a5603d82d0ced7416c29768fd1fcfc3c
Matt Cosentino <matt@learnedmedia.com>
2018-01-19 21:59:25 -0500

    settings page is using the dashbaord template now too

--- public/settings.php
+++ public/settings.php
@@ -5,19 +5,43 @@ session_start();
 if (!isset($_SESSION['username'])):
 	header('Location: login.php');
 endif;
+
+require "../config.php";
+require "../common.php";
+
 ?>
 
 <?php include "templates/header.php"; ?>
 
-<h3>SETTINGS</h3>
+<div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
+	
+	<?php include "templates/layout-header.php"; ?>
+	
+	<?php include "templates/sidebar.php"; ?>
 
-	<ul>
-		<li><a href="#"><strong>Update</strong></a> - edit your account.</li>
-		<li><a href="#"><strong>Update</strong></a> - edit a run.</li>
-		<li><a href="logout.php"><strong>Logout</strong></a></li>
-		<li><a href="#"><strong>Delete Account</strong></a></li>
-	</ul>
+	<main class="mdl-layout__content mdl-color--grey-100">
+		<div class="mdl-grid demo-content">
+			<div class="mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
+				
+				<h3>SETTINGS</h3>
 
-<a href="index.php">Back to home</a>
+				<ul>
+					<li><a href="#"><strong>Update</strong></a> - edit your account.</li>
+					<li><a href="#"><strong>Update</strong></a> - edit a run.</li>
+					<li><a href="logout.php"><strong>Logout</strong></a></li>
+					<li><a href="#"><strong>Delete Account</strong></a></li>
+				</ul>
+
+			</div>
+
+			<?php include "templates/demo-content.php"; ?>
+			
+			<?php include "templates/cards.php"; ?>
+
+		</div>
+	</main>
+</div>
+
+<?php include "templates/svg.php"; ?>
 
 <?php include "templates/footer.php"; ?>
