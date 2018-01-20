366d58d19322c98116e9cdcc347c92be1eb9f551
Matt Cosentino <matt@learnedmedia.com>
2018-01-20 16:48:40 -0500

    added d3 cdn script and got a json output of the user's run data

--- public/dashboard.php
+++ public/dashboard.php
@@ -22,7 +22,10 @@ try {
 	$statement->bindParam(':username', $username, PDO::PARAM_STR);
 	$statement->execute();
 
-	$result = $statement->fetchAll();
+	$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+	$jsonResults = json_encode($result);
+
 
 }
 
@@ -44,45 +47,7 @@ catch(PDOException $error) {
 		<div class="mdl-grid demo-content">
 			<div class="mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
 				
-				<?php if ($result && $statement->rowCount() > 0): ?>
-
-					<table>
-						<thead>
-							<tr>
-								<th>Date</th>
-								<th>Distance</th>
-								<th>Time</th>
-								<th>Pace</th>
-							</tr>
-						</thead>
-						<tbody>
-						
-						<?php 
-
-						foreach ($result as $row): 
-
-							$miles = escape($row["distance"]);
-							$run_time_in_seconds = time_to_seconds(escape($row["run_time"]));
-							$seconds_per_mile = $run_time_in_seconds / $miles;
-
-						?>
-							<tr>
-								<td><?php echo escape($row["run_date"]); ?></td>
-								<td><?php echo $miles; ?> miles</td>
-								<td><?php echo escape($row["run_time"]); ?></td>
-								<td><?php echo seconds_to_time($seconds_per_mile); ?></td>
-
-							</tr>
-						<?php endforeach; ?>
-
-						</tbody>
-					</table>
-					
-				<?php else: ?>
-
-					<blockquote>No results found for <?php echo $username ?>. Add a run <a href="addrun.php">here</a>.</blockquote>
-
-				<?php endif; ?>
+				<?php echo $jsonResults; ?>
 
 			</div>
 
