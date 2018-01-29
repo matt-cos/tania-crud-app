38ff214696c2bca8fc30ff1a8fc0cce6f810025b
Matt Cosentino <matt@learnedmedia.com>
2018-01-29 18:41:23 -0500

    got a graph functioning and showing php data

--- public/dashboard.php
+++ public/dashboard.php
@@ -37,6 +37,7 @@ catch(PDOException $error) {
 
 <?php include "templates/header.php"; ?>
 
+
 <div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
 	
 	<?php include "templates/layout-header.php"; ?>
@@ -45,6 +46,13 @@ catch(PDOException $error) {
 
 	<main class="mdl-layout__content mdl-color--grey-100">
 		<div class="mdl-grid demo-content">
+			
+			<div class="mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
+				
+				<svg id="visualisation" width="1000" height="500"></svg>
+
+			</div>
+
 			<div class="mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
 				
 				<?php echo $jsonResults; ?>
@@ -60,6 +68,110 @@ catch(PDOException $error) {
 </div>
 
 <?php include "templates/svg.php"; ?>
+
+<script>
+	// https://code.tutsplus.com/tutorials/building-a-multi-line-chart-using-d3js--cms-22935
+	// var data = [{
+	// 	"sale": "202",
+	// 	"year": "2000"
+	// }, {
+	// 	"sale": "215",
+	// 	"year": "2001"
+	// }, {
+	// 	"sale": "179",
+	// 	"year": "2002"
+	// }, {
+	// 	"sale": "199",
+	// 	"year": "2003"
+	// }, {
+	// 	"sale": "176",
+	// 	"year": "2010"
+	// }];
+
+	// var data2 = [{
+	// 	"sale": "152",
+	// 	"year": "2000"
+	// }, {
+	// 	"sale": "189",
+	// 	"year": "2002"
+	// }, {
+	// 	"sale": "179",
+	// 	"year": "2004"
+	// }, {
+	// 	"sale": "199",
+	// 	"year": "2006"
+	// }, {
+	// 	"sale": "134",
+	// 	"year": "2008"
+	// }, {
+	// 	"sale": "176",
+	// 	"year": "2010"
+	// }];
+
+	var phpData = <?php echo $jsonResults; ?>;
+
+	// console.log(data);
+	console.log(phpData);
+
+
+	var vis = d3.select("#visualisation"),
+	WIDTH = 1000,
+	HEIGHT = 500,
+	MARGINS = {
+		top: 20,
+		right: 20,
+		bottom: 20,
+		left: 50
+	},
+
+	xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([0,10]),
+	yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([0,10]),
+
+	xAxis = d3.svg.axis()
+		.scale(xScale),
+
+	yAxis = d3.svg.axis()
+		.scale(yScale)
+		.orient("left");
+
+	vis.append("svg:g")
+		.attr("class", "axis")
+		.attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
+		.call(xAxis);
+
+	vis.append("svg:g")
+		.attr("class", "axis")
+		.attr("transform", "translate(" + (MARGINS.left) + ",0)")
+		.call(yAxis);
+
+	var lineGen = d3.svg.line()
+		.x(function(d) {
+			return xScale(d.distance);
+		})
+		.y(function(d) {
+			return yScale(d.distance);
+		})
+		.interpolate("basis");
+
+	vis.append('svg:path')
+		.attr('d', lineGen(phpData))
+		.attr('stroke', 'green')
+		.attr('stroke-width', 2)
+		.attr('fill', 'none');
+
+	// vis.append('svg:path')
+	// 	.attr('d', lineGen(data))
+	// 	.attr('stroke', 'green')
+	// 	.attr('stroke-width', 2)
+	// 	.attr('fill', 'none');
+
+	// vis.append('svg:path')
+	// 	.attr('d', lineGen(data2))
+	// 	.attr('stroke', 'blue')
+	// 	.attr('stroke-width', 2)
+	// 	.attr('fill', 'none');
+
+</script>
 
 <!-- can use wunderground API for historical weather -->
 <!-- https://www.wunderground.com/weather/api/d/docs?d=data/history -->
--- public/templates/header.php
+++ public/templates/header.php
@@ -10,7 +10,8 @@
 		<link rel="stylesheet" href="css/style.css">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 		<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue_grey-orange.min.css" />
-		<script src="https://d3js.org/d3.v4.min.js"></script>
+		<!-- <script src="https://d3js.org/d3.v4.min.js"></script> -->
+		<script src="https://d3js.org/d3.v3.min.js"></script>
 		<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
 		<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
